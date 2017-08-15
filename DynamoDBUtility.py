6474ac1543b65c89dd85fe59d745bcde98979f42
Chege <d.chege711@gmail.com>
2017-08-15 16:36:34 -0400

    Add the boto3 credentials in the request body.

--- DynamoDBUtility.py
+++ DynamoDBUtility.py
@@ -9,7 +9,12 @@ Centralized location for handling all requests to Amazon's DynamoDB
 import boto3
 import os
 from botocore.exceptions import ClientError
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+dynamodb = boto3.resource(
+    'dynamodb',
+    aws_access_key_id = os.environ['aws_access_key_id'],
+    aws_secret_access_key = os.environ['aws_secret_access_key'],
+    region_name = 'us-east-1'
+)
 
 #_______________________________________________________________________________
 
