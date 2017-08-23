a0614815095603951d214abc8befcc0bf46be62b
Chege <d.chege711@gmail.com>
2017-08-23 16:21:27 -0400

    Remove unnecessary measurements from SensorPi.py

--- SensorPi.py
+++ SensorPi.py
@@ -45,25 +45,8 @@ def collectData(numberOfDataPoints, feature):
     data_all = np.zeros((numberOfDataPoints, feature + 1))
 
     for i in range(numberOfDataPoints):
-        # Record several parameters of interest
-        # device = {}
-        # device['cpuTemperature'] = os.popen('vcgencmd measure_temp').readline().replace("temp=","").replace("'C\n","")
-        #
-        # temp = {}
-        # temp['basedOnHumidity'] = sense.get_temperature_from_humidity()
-        # temp['basedOnPressure'] = sense.get_temperature_from_pressure()
-        #
-        # environment = {}
-        # environment['humidity'] = sense.get_humidity()
-        # environment['pressure'] = sense.get_pressure()
-        # environment['temperature'] = temp
-        #
-        # message = {}
-        # message['device'] = device
-        # message['environment'] = environment
-
+        
         tdataTime = time.time()
-
         data_all[i][0] = tdataTime
         data_all[i][1] = sense.get_pressure()
         data_all[i][2] = sense.get_humidity()
