0c61748f89b042cb595b3c6564ed527eed963158
princeton-edge-lab <32491622+princeton-edge-lab@users.noreply.github.com>
2017-10-27 10:16:53 -0400

    Update Button.py

--- Button.py
+++ Button.py
@@ -9,16 +9,16 @@ running the experiment.
 '''
 #_______________________________________________________________________________
 
+# tkinter was was previously named Tkinter in python 2
 import sys
-
-# Tkinter was was previously named tkinter in python 2
 version = sys.version_info.major
+print(version)
 if version == 2:
-	import tkinter as tk
-elif version == 3:
 	import Tkinter as tk
+elif version == 3:
+	import tkinter as tk
 else:
-	raise ImportError("This script needs either Python 2 or Python 3")
+raise ImportError("This script needs either Python 2 or Python 3")
 	
 import time
 import os
